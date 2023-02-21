c7c6f80a51a62831aca12da80dbcbe9c9d92d5c9
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2023-02-21 18:33:12 +0200

    Added Cross-Nation Interpretability Index Diff metrics

---
+++ metabase/dashboard/queries/Cross-Nation Interpretability Index Diff.sql
@@ -0,0 +1,50 @@
+WITH P AS (
+	SELECT testcase_id
+	  FROM public.test_participants
+     WHERE participant_role = 'Provider'
+     GROUP BY testcase_id
+    HAVING count(*) = 1
+	INTERSECT
+	SELECT testcase_id
+	  FROM public.test_participants
+	 WHERE participant_role = 'Consumer'
+	 GROUP BY testcase_id
+	HAVING count(1) >= 1
+)
+SELECT
+	100.0 * sum(
+	    CASE
+	        WHEN p.exercise_cycle = 'CWIX 2022' AND c.consumer_participant_result = 'Success' THEN 1
+	        ELSE 0
+	    END
+	) / sum(
+	    CASE
+	        WHEN p.exercise_cycle = 'CWIX 2022' THEN 1
+	        ELSE 0
+	    END
+	) - 100.0 * sum(
+	    CASE
+	        WHEN p.exercise_cycle = 'CWIX 2021' AND c.consumer_participant_result = 'Success' THEN 1
+	        ELSE 0
+	    END
+	) / sum(
+	    CASE
+	        WHEN p.exercise_cycle = 'CWIX 2021' THEN 1
+	        ELSE 0
+	    END
+	) AS diff
+  FROM (
+    SELECT tp.testcase_id,
+           tp.exercise_cycle,
+           tp.participant_result as provider_participant_result
+	  FROM public.test_participants tp
+	  JOIN P p ON p.testcase_id = tp.testcase_id
+	  WHERE tp.participant_role = 'Provider'
+) p LEFT JOIN (
+    SELECT tp.testcase_id,
+           tp.exercise_cycle,
+           tp.participant_result as consumer_participant_result
+	  FROM public.test_participants tp
+	  JOIN P p ON p.testcase_id = tp.testcase_id
+	 WHERE tp.participant_role = 'Consumer'
+) c ON c.testcase_id = p.testcase_id;
